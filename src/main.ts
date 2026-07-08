1b08fabe828fe54cad98f11ff00db3c09a18a7cb
Kevin Ignatius <53389426+ZetsubouCode@users.noreply.github.com>
2026-07-09 01:08:33 +0700

    Render selected adaptations section

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { renderCaseStudies, renderCommissionCta } from "./components/caseStudies
 import { renderCollectionCards } from "./components/collectionCards";
 import { renderHero } from "./components/hero";
 import { ShowcaseGallery } from "./components/showcaseGallery";
+import { renderSelectedAdaptations } from "./components/selectedAdaptations";
 import { renderFocus, renderStats } from "./components/stats";
 import { renderWorkflow } from "./components/workflow";
 import { renderBrandLabel } from "./lib/brandIcons";
@@ -40,6 +41,7 @@ function boot(): void {
       <main>
         ${renderFocus(site)}
         ${renderStats(site)}
+        ${renderSelectedAdaptations(site, collections)}
         ${renderCollectionCards(collections, site.ratingModes)}
         <section class="section section--showcase" id="showcase"></section>
         ${renderWorkflow(site)}
